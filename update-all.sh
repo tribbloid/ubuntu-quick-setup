4f2da9c6a2f4eb165d4125c9eec4c8e55d2b6545
Peng Cheng <pc175@uowmail.edu.au>
2019-07-12 15:57:33 -0400

    first commit with submodules

---
+++ update-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git config --global credential.helper store
+git pull --rebase --autostash
+
+#git submodule init
+####  sometimes revision track from parent is broken, need to use update --remote
+git submodule foreach git fetch
+git submodule update --init --recursive --force
+
