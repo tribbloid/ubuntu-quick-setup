951764dd9b93a197700fbc63ebf312ea90a916a7
Peng Cheng <pc175@uowmail.edu.au>
2019-09-10 20:17:11 -0400

    re-snapshot add dryrun option and condarc

---
+++ packages/recover-dryrun.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt-clone show-diff latest.apt-clone.tar.gz
+
+#conda env update --file conda-env.yml
+
--- packages/snapshot.sh
+++ packages/snapshot.sh
@@ -3,6 +3,6 @@
 DATE=$(date --iso-8601=second)
 
 #apt clone
-sudo apt-clone clone latest > apt-report_$DATE.log
+apt-clone clone latest > apt-report_$DATE.log
 
 conda env export -n root > conda-env.yml
