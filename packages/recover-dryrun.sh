89fefce29551a264b5b8b6ede3e0a308e3deeb44
Peng Cheng <pc175@uowmail.edu.au>
2020-11-07 17:39:23 -0500

    improve conda export

--- packages/recover-dryrun.sh
+++ packages/recover-dryrun.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+echo ""
 echo "###############"
 echo "#     apt     #"
 echo "###############"
+echo ""
 
 apt-clone show-diff latest.apt-clone.tar.gz
 
+echo ""
 echo "###############"
 echo "#    conda    #"
 echo "###############"
+echo ""
 
 conda compare -n root conda-env.yml
 
