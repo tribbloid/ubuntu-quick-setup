da6702e2fb120a4d910df2672c5da6dbef4c283c
Peng Cheng <pc175@uow.edu.au>
2021-06-22 19:30:55 -0400

    add apt-key back and restore upgrade to nvidia-465.31

--- packages/recover.sh
+++ packages/recover.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt-key add latest.keys # TODO: deprecated, will be removed after 2022
+
 sudo apt-clone restore latest.apt-clone.tar.gz
 
 conda env update -n root --file conda-env.yml
--- packages/snapshot.sh
+++ packages/snapshot.sh
@@ -2,6 +2,8 @@
 
 DATE=$(date --iso-8601=second)
 
+sudo apt-key exportall > latest.keys
+
 #apt clone
 apt-clone clone latest > apt-report_$DATE.log
 
