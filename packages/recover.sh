a1e787eb666620a5990a8451df41a5eca506cb64
peng <pc175@uow.edu.au>
2021-07-23 18:01:15 -0400

    append conda-forge channel automatically

--- packages/recover.sh
+++ packages/recover.sh
@@ -4,5 +4,5 @@ sudo apt-key add latest.keys # TODO: deprecated, will be removed after 2022
 
 sudo apt-clone restore latest.apt-clone.tar.gz
 
+conda config --append channels conda-forge
 conda env update -n root --file conda-env.yml
-
