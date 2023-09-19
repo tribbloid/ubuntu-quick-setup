f1a0dae5c73fe746eb68daa3bc94dc3c4fa178c4
peng <peng.cheng@acyclic.ai>
2023-09-19 17:27:53 -0400

    keys moved into __snapshot dir

--- packages/recover.sh
+++ packages/recover.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#sudo apt-key add latest.keys # TODO: deprecated, will be removed after 2022
+#sudo apt-key add __snapshot/latest.keys # TODO: deprecated, will be removed after 2022
 
 sudo apt-clone restore __snapshot/latest.apt-clone.tar.gz
 #sudo apt-clone restore-new-distro "latest.apt-clone.tar.gz" "22.04 LTS"
--- packages/snapshot.sh
+++ packages/snapshot.sh
@@ -5,8 +5,8 @@ mkdir -p __snapshot
 
 DATE=$(date --iso-8601=second)
 
-apt-key exportall > latest.keys
-apt-key list | grep "uid" > latest.keys.list
+apt-key exportall > __snapshot/latest.keys
+apt-key list | grep "uid" > __snapshot/latest.keys.list
 
 #apt clone
 apt-clone clone __snapshot/latest > log/apt-report_$DATE.log
