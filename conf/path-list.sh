a3e9612dafd3fef86de6b5190bd5923c7cedb8be
peng <peng.cheng@acyclic.ai>
2024-09-15 19:21:41 -0400

    only cherrypick 3 shell extensions

--- conf/path-list.sh
+++ conf/path-list.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-export PATHS=("/org/gnome/shell/extensions/")
+export PATHS=(
+"/org/gnome/shell/extensions/openweather/"
+"/org/gnome/shell/extensions/system-monitor/"
+"/org/gnome/shell/extensions/dash-to-dock/"
+)
