aff1c929f639426f086869497a98484d9d868b72
Peng Cheng <peng.cheng@acyclic.ai>
2025-04-20 15:39:42 -0400

    requirements files are gone in favour of pyproject
    snapshot now generate separate files for id-only and all-inclusive list for snap & flatpak

--- packages/snapshot.sh
+++ packages/snapshot.sh
@@ -5,7 +5,7 @@ mkdir -p __snapshot
 
 DATE=$(date --iso-8601=second)
 
-apt-key exportall > __snapshot/latest.keys
+#apt-key exportall > __snapshot/latest.keys
 apt-key list | grep "uid" > __snapshot/latest.keys.list
 
 #apt clone
@@ -16,6 +16,8 @@ apt-mark showmanual > __snapshot/apt.csv
 conda env export -n root --from-history --no-builds > __snapshot/conda-env.yml
 conda env export -n root > __snapshot/conda-env-full.yml
 
-flatpak list --app > __snapshot/flatpak.csv
+flatpak list --app --columns=application > __snapshot/flatpak.csv
+flatpak list --app > __snapshot/flatpak-full.csv
 
-snap list > __snapshot/snap.csv
+snap list | awk 'NR>1 && $3 != "canonical" && $1 !~ /^(core|core[0-9]+|snapd|gnome-|gtk-|snap-store)/ {print $1}' > __snapshot/snap.csv
+snap list > __snapshot/snap-full.csv
