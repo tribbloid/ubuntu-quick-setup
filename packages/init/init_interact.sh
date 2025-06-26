f51ebbb81ce7a9e03b33dffd4fa6d28dc0a04575
Peng Cheng <peng.cheng@acyclic.ai>
2025-06-26 12:48:20 -0400

    minor tweaking

--- packages/init/init_interact.sh
+++ packages/init/init_interact.sh
@@ -5,6 +5,8 @@ curl https://elan.lean-lang.org/elan-init.sh -sSf | sh
 # On x86-64 (aka AMD64)
 curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
 chmod +x cs
 ./cs setup
 
--- packages/init/mutter-hack.sh
+++ packages/init/mutter-hack.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sleep 10 && (pkill -HUP mutter-x11-fram & ~/upgrade)
+sleep 10
+
+pkill -HUP mutter-x11-fram
+
+/usr/bin/gnome-extensions disable ubuntu-appindicators@ubuntu.com
+/usr/bin/gnome-extensions enable ubuntu-appindicators@ubuntu.com
+
+flatpak update
