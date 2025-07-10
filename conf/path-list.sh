dd805b826e1bdd26379b807bcafc4731bac4908d
Peng Cheng <peng.cheng@acyclic.ai>
2025-07-10 19:17:34 -0400

    conf now export all extensions

--- conf/path-list.sh
+++ conf/path-list.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 export PATHS=(
-"/org/gnome/shell/extensions/openweather/"
-"/org/gnome/shell/extensions/system-monitor/"
-"/org/gnome/shell/extensions/dash-to-dock/"
+"/org/gnome/shell/extensions/"
 )
